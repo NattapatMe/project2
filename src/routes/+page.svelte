dd45956bc6395ce6bbd609ae796e84b8362d6578
Famai <famai.ntp@gmail.com>
2025-06-27 15:52:24 +0700

    add style in root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
-<div>
-    <h1>User Manager</h1>
+<div class="text-center">
+    <h1 class="text-5xl pb-10 pt-10">User Manager</h1>
     <form>
 
-        <div>
+        <div class="pt-5">
             <label for="username">Username:</label>
-            <input type="text">
+            <input class="border" type="text">
         </div>
 
-        <div>
+        <div class="pt-5">
             <label for="password">Password:</label>
-            <input type="text">
+            <input class="border" type="text">
         </div>
 
-        <div>
-            <button type="button">Login</button>
+        <div class="pt-10">
+            <button type="button" id="login">Login</button>
         </div>
     </form>
 </div>
