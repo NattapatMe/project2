14d7b78ef626c069360c9dee1c27ef3aca2e082d
Famai <famai.ntp@gmail.com>
2025-06-27 15:32:43 +0700

    setup page, install tailwind, insert component on root page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div>
+    <h1>User Manager</h1>
+    <form>
+
+        <div>
+            <label for="username">Username:</label>
+            <input type="text">
+        </div>
+
+        <div>
+            <label for="password">Password:</label>
+            <input type="text">
+        </div>
+
+        <div>
+            <button type="button">Login</button>
+        </div>
+    </form>
+</div>
